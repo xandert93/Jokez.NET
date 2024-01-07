e71f4d7dc12e00cc06e745970030e63f196a5ab8
Alex <xandert.93@outlook.com>
2024-01-07 01:30:51 +0000

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Jokez.Models;
+
+namespace Jokez.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Jokez.Models.Joke> Joke { get; set; } = default!;
+    }
+}
